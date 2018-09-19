2cd093faf611d04ca09f26e50aae18590cc36379
Ivan Ushmorov <ivan.ushmorov@raysono.com>
2018-09-19 09:20:38 +0200

    minor changes

--- src/utils/apiKey.js
+++ src/utils/apiKey.js
@@ -1,2 +1,3 @@
 let apiKey = 'XlZO9B98Aehw1RkeFsmFMjjwnG1pVfllc1YHz_P_jn70jFQc1_FMboomwkD7wnuxzHYTPLaWJJwfyFUmD-Nn79E0DYQyx_VPD6yIfwfMHjIgVwgfRdd-i4M4XiqdW3Yx';
+//let googlePlacesKey = 'AIzaSyBqZf9emaPpFEEfPdxmuM0PjlB_MiZyD5U'
 module.exports = apiKey;
