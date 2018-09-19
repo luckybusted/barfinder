ddf8324e4d72651b1615bc01e294b9549e5bfe80
Ivan Ushmorov <ivan.ushmorov@raysono.com>
2018-09-19 11:05:52 +0200

    added content to page-footer

--- src/components/PageBasics/PageFoot.js
+++ src/components/PageBasics/PageFoot.js
@@ -2,21 +2,17 @@ import React, {Component} from 'react';
 
 class PageFoot extends Component {
 
-    componentDidMount() {
-
-    }
-
-
     render() {
-
 
         return (
             <div className="footer container" id="footer">
-                PAGE FOOTER
+                <div className="row">
+                    <div className="col-6">Bar-Finder &copy;2018</div>
+                    <div className="col-6">powered by <span className="bf-yelp"></span></div>
+                </div>
             </div>
         );
     }
 }
-
 
 export default PageFoot;
