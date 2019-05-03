1158e3750da36e7e46895c86c3d999599090da01
Ivan Ushmorov <ivan.ushmorov@raysono.com>
2019-05-03 12:06:30 +0200

    minor styling changes

--- src/components/PageBasics/PageFoot.js
+++ src/components/PageBasics/PageFoot.js
@@ -5,7 +5,7 @@ class PageFoot extends Component {
     render() {
 
         return (
-            <div className="footer container" id="footer">
+            <div className="footer" id="footer">
                 <div className="row">
                     <div className="col-6">Bar-Finder &copy; 2018</div>
                     <div className="col-6 text-right">powered by <span className="bf-yelp" />Yelp</div>
