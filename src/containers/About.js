c1312a2267537f3619c878c41a064362d1d0da6e
luckybusted <ivan.ushmorov@gmail.com>
2019-08-15 00:44:06 +0200

    cleanup

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
 class About extends Component {
-    render() {
-        return (
-            <div>THIS WILL BE THE ABOUT</div>
-        );
-    }
+  render() {
+    return <div>THIS WILL BE THE ABOUT</div>;
+  }
 }
 
 export default About;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,67 +1,72 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import Loader from "../modules/Loader";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Loader from '../modules/Loader';
 
 let ReactRedux = require('react-redux');
 let actions = require('../actions/actions');
 
 class Home extends Component {
+  componentWillMount() {
+    this.props.setEnv('Home');
+  }
 
-    componentWillMount() {
-        this.props.setEnv('Home');
-    }
+  componentWillUnmount() {
+    this.props.setEnv('');
+  }
 
-    componentWillUnmount() {
-        this.props.setEnv('');
-    }
+  render() {
+    let showSpinner = this.props.showLocationLoader,
+      noLocation = this.props.noLocation;
 
-    render() {
-
-        let showSpinner = this.props.showLocationLoader,
-            noLocation = this.props.noLocation;
-
-        return (
-            <div className="container">
-                {noLocation &&
-                <div className='alert-danger p-4 d-flex align-content-center justify-content-center'>
-                    Wir konnten Ihren Standort nicht ermitteln. Bitte Versuchen Sie es erneut.
-                </div>
-                }
-                {!noLocation &&
-                <div className="row">
-                    <div className="col-sm-6 mb-4">
-                        <Link to={{
-                            pathname: '/LocationDetail',
-                            state: {
-                                feelingLucky: true
-                            }
-                        }} className="btn btn-block text-uppercase p-4 btn-primary btn-lg">Auf gut Glück</Link>
-                    </div>
-                    <div className="col-sm-6">
-                        <Link to="/LocationsOverview"
-                              className="btn btn-block text-uppercase p-4 btn-primary btn-lg">Bars Suchen</Link>
-                    </div>
-                </div>
-                }
-                {showSpinner &&
-                <Loader/>
-                }
-                {/*
+    return (
+      <div className='container'>
+        {noLocation && (
+          <div className='alert-danger p-4 d-flex align-content-center justify-content-center'>
+            Wir konnten Ihren Standort nicht ermitteln. Bitte Versuchen Sie es
+            erneut.
+          </div>
+        )}
+        {!noLocation && (
+          <div className='row'>
+            <div className='col-sm-6 mb-4'>
+              <Link
+                to={{
+                  pathname: '/LocationDetail',
+                  state: {
+                    feelingLucky: true
+                  }
+                }}
+                className='btn btn-block text-uppercase p-4 btn-primary btn-lg'
+              >
+                Auf gut Glück
+              </Link>
+            </div>
+            <div className='col-sm-6'>
+              <Link
+                to='/LocationsOverview'
+                className='btn btn-block text-uppercase p-4 btn-primary btn-lg'
+              >
+                Bars Suchen
+              </Link>
+            </div>
+          </div>
+        )}
+        {showSpinner && <Loader />}
+        {/*
                 <div>You are here: {this.props.userLocation.latitude + ', ' + this.props.userLocation.longitude}</div>
                 */}
-
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default ReactRedux.connect(
-    (state) => ({
-        userLocation: state.dataReducer.userLocation,
-        showLocationLoader: state.dataReducer.showLocationLoader,
-        noLocation: state.dataReducer.noLocation
-    }),
-    (dispatch) => ({
-        setEnv: (env) => dispatch(actions.setEnv(env))
-    })
+  state => ({
+    userLocation: state.dataReducer.userLocation,
+    showLocationLoader: state.dataReducer.showLocationLoader,
+    noLocation: state.dataReducer.noLocation
+  }),
+  dispatch => ({
+    setEnv: env => dispatch(actions.setEnv(env))
+  })
 )(Home);
