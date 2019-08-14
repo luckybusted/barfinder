bc92cc6bf47a08fbffad402e6e3c24cedf0af4f3
luckybusted <ivan.ushmorov@gmail.com>
2019-08-15 00:51:12 +0200

    test

--- src/containers/About.js
+++ src/containers/About.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class About extends Component {
   render() {
-    return <div>THIS WILL BE THE ABOUT</div>;
+    return <div>THIS WILL BE THE ABOUT TEST</div>;
   }
 }
 
